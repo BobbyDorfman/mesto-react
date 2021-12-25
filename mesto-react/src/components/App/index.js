e76f54410435d49213292bf9dc3194134a07aa15
Sergey Fedorenko <fedorenko_s_a@mail.ru>
2021-12-25 21:32:23 +0300

    feat: портировать стили

--- mesto-react/src/components/App/index.js
+++ mesto-react/src/components/App/index.js
@@ -1,29 +1,37 @@
-import './App.css';
+import '../../index.css';
+import logoHeader from '../../image/Vector.svg';
+import editIcon from '../../image/Edit.svg';
+import addButtonIcon from '../../image/Add_button.svg';
+import closeIcon from '../../image/Close_Icon.svg';
+import deleteIcon from '../../image/delete_2.svg';
+import likeIcon from '../../image/Like.svg';
 
 function App() {
     return (
     <div className="body">
         <div className="page">
             <header className="header">
-                <img className="header__logo" src="./image/Vector.svg" alt="Логотип"/>
+                <img className="header__logo" src={logoHeader} alt="Логотип"/>
             </header>
 
             <main>
-                <div className="profile__avatar-container">
-                    <button className="profile__edit-avatar" type="button"></button>
-                    <img className="profile__avatar" src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif" alt="Аватар профиля" />
-                </div>
+                <section className="profile">
+                    <div className="profile__avatar-container">
+                        <button className="profile__edit-avatar" type="button"></button>
+                        <img className="profile__avatar" src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif" alt="Аватар профиля" />
+                    </div>
 
-                <div className="profile__info">
-                    <h1 className="profile__title"></h1>
-                    <button className="profile__edit-button" type="button">
-                    <img className="profile__pencil" src="./image/Edit.svg" alt="Элемент редактирования" />
+                    <div className="profile__info">
+                        <h1 className="profile__title"></h1>
+                        <button className="profile__edit-button" type="button">
+                            <img className="profile__pencil" src={editIcon} alt="Элемент редактирования" />
+                        </button>
+                        <p className="profile__subtitle">Идет загрузка...</p>
+                    </div>
+                    <button className="profile__add-button" type="button">
+                        <img className="profile__plus" src={addButtonIcon} alt="Знак - добавить" />
                     </button>
-                    <p className="profile__subtitle">Идет загрузка...</p>
-                </div>
-                <button className="profile__add-button" type="button">
-                    <img className="profile__plus" src="./image/Add_button.svg" alt="Знак - добавить" />
-                </button>
+                </section>
 
                 <section className="elements" aria-label="Фотоальбом"></section>
             </main>
@@ -36,7 +44,7 @@ function App() {
         <div className="popup popup_type_edit">
             <div className="edit-form">
                 <button className="popup__close edit-form__close-icon" type="button">
-                    <img className="popup__close-image" src="./image/Close_Icon.svg" alt="Иконка - закрыть" />
+                    <img className="popup__close-image" src={closeIcon} alt="Иконка - закрыть" />
                 </button>
                 <h2 className="edit-form__title">Редактировать профиль</h2>
                 <form classname="popup__form popup__form_type_edit" name="form_edit" noValidate>
@@ -54,7 +62,7 @@ function App() {
         <div class="popup popup_type_add">
             <div class="adding-cards">
                 <button class="popup__close adding-cards__close-icon" type="button">
-                    <img class="popup__close-image" src="<%=require('./image/Close_Icon.svg')%>" alt="Иконка - закрыть" />
+                    <img class="popup__close-image" src={closeIcon} alt="Иконка - закрыть" />
                 </button>
                 <h2 class="adding-cards__title">Новое место</h2>
                 <form class="popup__form popup__form_type_adding" name="form_add" novalidate>
@@ -72,7 +80,7 @@ function App() {
         <div class="popup popup_type_image">
             <div class="image-in-full">
                 <button class="popup__close image-in-full__close-icon" type="button">
-                    <img class="popup__close-image" src="./image/Close_Icon.svg" alt="Иконка - закрыть" />
+                    <img class="popup__close-image" src={closeIcon} alt="Иконка - закрыть" />
                 </button>
                 <figure class="image-in-full__content">
                     <img class="image-in-full__image" src="src" alt="alt" />
@@ -83,7 +91,7 @@ function App() {
         <div class="popup popup_type_delete">
             <div class="delete-cards">
                 <button class="popup__close delete-cards__close-icon" type="button">
-                    <img class="popup__close-image" src="./image/Close_Icon.svg" alt="Иконка - закрыть" />
+                    <img class="popup__close-image" src={closeIcon} alt="Иконка - закрыть" />
                 </button>
                 <h2 class="delete-cards__title">Вы уверены?</h2>
                 <form class="popup__form popup__form_type_delete" name="form_delete" novalidate>
@@ -94,7 +102,7 @@ function App() {
         <div class="popup popup_type_avatar">
             <div class="edit-avatar">
                 <button class="popup__close edit-avatar__close-icon" type="button">
-                    <img class="popup__close-image" src="./image/Close_Icon.svg" alt="Иконка - закрыть" />
+                    <img class="popup__close-image" src={closeIcon} alt="Иконка - закрыть" />
                 </button>
                 <h2 class="edit-avatar__title">Обновить аватар</h2>
                 <form class="popup__form popup__form_type_avatar" name="form_avatar" novalidate>
@@ -109,21 +117,21 @@ function App() {
         <template id="card-template">
             <article class="element">
                 <button class="element__button-delete" type="button">
-                    <img class="element__delete" src="./image/delete_2.svg" alt="Иконка удаления карточки" />
+                    <img class="element__delete" src={deleteIcon} alt="Иконка удаления карточки" />
                 </button>
                 <img class="element__image" src="src" alt="alt" />
                 <div class="element__down">
                     <h2 class="element__title"></h2>
                     <div class="element__likes">
                         <button class="element__button" type="button">
-                            <img class="element__like" src="./image/Like.svg" alt="Лайк" />
+                            <img class="element__like" src={likeIcon} alt="Лайк" />
                         </button>
                         <span class="element__like-counter">0</span>
                     </div>
                 </div>
             </article>
         </template>
-        
+
     </div>
   );
 }
